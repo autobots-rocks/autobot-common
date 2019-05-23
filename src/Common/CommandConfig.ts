27920a009a55d8fbb2dc18badac5d7917e859242
Matthew Davis <matthew@matthewdavis.io>
2019-05-23 09:59:51 -0500

    first commit

---
+++ src/Common/CommandConfig.ts
@@ -0,0 +1,15 @@
+import { CommandParam } from './CommandParam';
+import { Event }        from './Event';
+
+export class CommandConfig {
+
+    public event: Event;
+    public name?: string;
+    public aliases?: string[];
+    public group?: string;
+    public description?: string;
+    public params?: CommandParam[];
+    public roles?: string[];
+    public entities?: any[];
+
+}
